af409a58948ea177c8124149303933534ed1c2cd
micstephenson <159960082+micstephenson@users.noreply.github.com>
2025-10-07 10:59:54 +0100

    Exercise 1

---
+++ CodingGauntletExercises/CodingGauntletExercises/Program.cs
@@ -0,0 +1,33 @@
+﻿public class BankAccount
+{
+    public string Name { get; set; }
+    public double Balance { get; set; }
+    public string Email { get; set; }
+
+    public BankAccount(string name, double balance, string email)
+    {
+        Name = name;
+        Balance = balance;
+        Email = email;
+    }
+
+    public void AccountDetails()
+    {
+        Console.WriteLine($"Account Holder: {Name}, Balance: {Balance}, Email: {Email}");
+    }
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BankAccount account1 = new BankAccount("Mic", 1234.0, "Mic@email.com");
+        BankAccount account2 = new BankAccount("Oli", 2345.0, "Oli@email.com");
+        BankAccount account3 = new BankAccount("Ben", 3456.0, "Ben@email.com");
+
+        account1.AccountDetails();
+        account2.AccountDetails();
+        account3.AccountDetails();
+    }
+}
