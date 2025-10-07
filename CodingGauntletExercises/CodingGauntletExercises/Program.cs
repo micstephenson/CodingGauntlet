55e4758dab3e1f63c0ae3bc1053392466346d0e4
micstephenson <159960082+micstephenson@users.noreply.github.com>
2025-10-07 11:21:44 +0100

    Loop Created

--- CodingGauntletExercises/CodingGauntletExercises/Program.cs
+++ CodingGauntletExercises/CodingGauntletExercises/Program.cs
@@ -26,8 +26,11 @@ class Program
         BankAccount account2 = new BankAccount("Oli", 2345.0, "Oli@email.com");
         BankAccount account3 = new BankAccount("Ben", 3456.0, "Ben@email.com");
 
-        account1.AccountDetails();
-        account2.AccountDetails();
-        account3.AccountDetails();
+        var accounts = new List<BankAccount> { account1, account2, account3 };
+
+        for (int i = 0; i < accounts.Count; i++)
+        {
+            accounts[i].AccountDetails();
+        }
     }
 }
--- CodingGauntletExercises/CodingGauntletExercises/obj/Debug/net8.0/CodingGauntletExercises.AssemblyInfo.cs
+++ CodingGauntletExercises/CodingGauntletExercises/obj/Debug/net8.0/CodingGauntletExercises.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CodingGauntletExercises")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af409a58948ea177c8124149303933534ed1c2cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("CodingGauntletExercises")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CodingGauntletExercises")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
